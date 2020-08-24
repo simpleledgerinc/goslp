14cd0de12d5ddc8de1fa9162816b54d73fc4b9a5
James Cramer <jcramer@simpleledger.io>
2020-08-24 14:55:05 -0400

    add two new address tests

--- address_test.go
+++ address_test.go
@@ -1,10 +1,13 @@
 package goslp
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
 	"testing"
 
 	"github.com/gcash/bchd/chaincfg"
+	"github.com/gcash/bchutil"
 )
 
 func TestDecodeSlpAddress(t *testing.T) {
@@ -43,5 +46,40 @@ func TestCashAddr2FailedDecodeAddressMainnet(t *testing.T) {
 	_, err := DecodeAddress(addrStr, &chaincfg.MainNetParams)
 	if err == nil {
 		t.Fatal(err)
+	}
+}
+
+func TestConvertP2pkhBchToSlpAddress(t *testing.T) {
+	addrStr := "qprqzzhhve7sgysgf8h29tumywnaeyqm7y6e869uc6"
+	params := &chaincfg.Params{
+		SlpAddressPrefix:  "simpleledger",
+		CashAddressPrefix: "bitcoincash",
+	}
+	addr, _ := bchutil.DecodeAddress(addrStr, params)
+	hash := addr.(*bchutil.AddressPubKeyHash).Hash160()
+
+	slpAddr, _ := NewAddressPubKeyHash(hash[:], params)
+	if slpAddr.String() != "qprqzzhhve7sgysgf8h29tumywnaeyqm7ykzvpsuxy" {
+		t.Fatal("incorrect conversion from cashAddr to bchAddr")
+	}
+}
+
+func TestConvertP2shBchToSlpAddress(t *testing.T) {
+	addrStr := "pzmj0ueqasnsw80a26th5t2gsz5evcxsps2tavljvp"
+	params := &chaincfg.Params{
+		SlpAddressPrefix:  "simpleledger",
+		CashAddressPrefix: "bitcoincash",
+	}
+	addr, _ := bchutil.DecodeAddress(addrStr, params)
+	hash := addr.(*bchutil.AddressScriptHash).Hash160()
+
+	expHash, _ := hex.DecodeString("0e13f9ce9a6be9653d812a030e936767b503debe")
+	if bytes.Equal(expHash, hash[:]) {
+		t.Fatal("incorrect hash decoded from bchutil")
+	}
+
+	slpAddr, _ := NewAddressScriptHashFromHash(hash[:], params)
+	if slpAddr.String() != "pzmj0ueqasnsw80a26th5t2gsz5evcxspsxskh2jjl" {
+		t.Fatal("incorrect conversion from cashAddr to bchAddr")
 	}
 }
